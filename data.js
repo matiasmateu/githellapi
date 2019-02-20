03645b4a5f673561d60274c9f6c1ad5fea31fe98
Matias <matias_mateu@icloud.com>
2019-02-20 13:44:11 +0100

    GamePlayers Data

---
+++ data.js
@@ -0,0 +1,13 @@
+export let game = {
+    isRunning :false,
+}
+
+export let player1 = {
+    x : 0,
+    y : 0
+}
+
+export let player2 = {
+    x : 0,
+    y : 0
+}
--- index.js
+++ index.js
@@ -7,6 +7,23 @@ var server = http.createServer(function (request, response) {
     response.end();
 });
 
+// TEMPORAL DATA
+let game = {
+    isRunning :false,
+    gravity:0.3,
+    velocity:1
+}
+
+let player1 = {
+    x : 100,
+    y : 50
+}
+
+let player2 = {
+    x : 300,
+    y : 50
+}
+
 server.listen(8080, function () {
     console.log((new Date()) + ' Server is listening on port 8080');
 });
@@ -52,15 +69,17 @@ wsServer.on('request', function (request) {
     console.log((new Date()) + ' Connection accepted!.');
     connection.on('message', function (message) {
         var data = JSON.parse(message.utf8Data)
+        console.log(data)
         if (data.type === 'Login') {
-            broadcast({type:"USER_CONNECTED",payload:data.user})
+            broadcast({type:"USER_CONNECTED",payload:data.data})
         }
         if (data.type === 'Logout') {
-            broadcast({type:"USER_DISCONNECTED",payload:data.user})
+            broadcast({type:"USER_DISCONNECTED",payload:data.data})
         }
-        if (data.type === 'NEW_GAME') {
-            console.log("NEW_GAME_STARTS_NOW")
-            broadcast({type:"NEW_GAME",payload:data.game})
+        if (data.type === 'NEW_GAME_REQUEST') {
+            broadcast({type:"NEW_GAME",payload:game})
+            broadcast({type:"UPDATE_PLAYER1",payload:player1})
+            broadcast({type:"UPDATE_PLAYER2",payload:player2})
         }
 
         /*
