aa69046567ddfca5d585647f572330443908547f
Matias <matias_mateu@icloud.com>
2019-02-21 15:45:35 +0100

    Platforms Updated from Server

---
+++ Platform.js
@@ -0,0 +1,13 @@
+export class Platform {
+    constructor(x, y) {
+        this.X = ~~x;
+        this.Y = y;
+        this.firstColor = '#FF8C00';
+        // this.secondColor = '#EEEE00';
+    }
+
+    draw(ctx) {
+        // ctx.fillStyle = this.firstColor;
+        ctx.fillRect(this.X,this.Y,100,20);
+    }
+}
